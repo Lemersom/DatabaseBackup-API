a886ec4e067856adfeb6bd62ebb6b73d2efb9c9a
lemersom <lemerfilho@gmail.com>
2024-12-16 15:45:25 -0300

    debug csvUtil and backup functions

--- src/main/java/com/example/backupvault/service/BackupService.java
+++ src/main/java/com/example/backupvault/service/BackupService.java
@@ -2,19 +2,17 @@ package com.example.backupvault.service;
 
 import com.example.backupvault.model.DatabaseConfigModel;
 import com.example.backupvault.util.CsvUtil;
-import com.example.backupvault.util.FileUtil;
-import com.example.backupvault.util.JsonUtil;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class BackupService {
@@ -30,27 +28,21 @@ public class BackupService {
 
         try {
             connection = createConnection(dbConfig);
-            connection.setAutoCommit(false);
+            connection.setAutoCommit(false); // Disable auto-commit for transactional integrity
 
-            FileUtil.createDirectoriesIfNotExists(databaseBackupDirectory);
+            Files.createDirectories(Paths.get(databaseBackupDirectory));
 
-            List<String> tables = getTableNames(connection);
+            List<String> tableNames = getTableNames(connection);
 
-            for(String tableName : tables) {
+            for(String tableName : tableNames) {
                 // Data
-                List<Map<String, Object>> tableData = getTableData(connection, tableName);
-                String dataFilePath = databaseBackupDirectory + File.separator + tableName + "-data";
-                JsonUtil.writeToJsonFile(dataFilePath + ".json", tableData);
-                CsvUtil.writeDataToCsvFile(dataFilePath + ".csv", tableData);
+                backupTableData(connection, tableName, databaseBackupDirectory);
 
                 // Schema
-                Map<String, String> tableSchema = getTableSchema(connection, tableName);
-                String schemaFilePath = databaseBackupDirectory + File.separator + tableName + "-schema";
-                JsonUtil.writeToJsonFile(schemaFilePath + ".json", tableSchema);
-                CsvUtil.writeSchemaToCsvFile(schemaFilePath + ".csv", tableSchema);
+                backupTableSchema(connection, tableName, databaseBackupDirectory);
             }
 
-            connection.commit();
+            connection.commit(); // Commit changes if everything is successful
 
         } catch (Exception e) {
             if(connection != null) {
@@ -70,6 +62,56 @@ public class BackupService {
         return databaseBackupDirectory;
     }
 
+    private void backupTableData(Connection connection, String tableName, String outputDir) throws SQLException, IOException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM " + tableName);
+
+        List<String[]> data = new ArrayList<>();
+        ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+        int columnCount = resultSetMetaData.getColumnCount();
+
+        while (resultSet.next()) {
+            String[] row = new String[columnCount];
+            for (int i = 0; i < columnCount; i++) {
+                row[i] = resultSet.getString(i + 1);
+            }
+            data.add(row);
+        }
+
+        String[] headers = new String[columnCount];
+        for (int i = 0; i < columnCount; i++) {
+            headers[i] = resultSetMetaData.getColumnName(i + 1);
+        }
+
+        CsvUtil.writeToCsv(outputDir + File.separator + tableName + "_data.csv", data, headers);
+    }
+
+    private void backupTableSchema(Connection connection, String tableName, String outputDir) throws SQLException, IOException {
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet columns = metaData.getColumns(null, null, tableName, null);
+
+        List<String[]> schema = new ArrayList<>();
+        while(columns.next()) {
+            String columnName = columns.getString("COLUMN_NAME");
+            String dataType = columns.getString("TYPE_NAME");
+            schema.add(new String[]{columnName, dataType});
+        }
+
+        CsvUtil.writeToCsv(outputDir + File.separator + tableName + "_schema.csv", schema, new String[]{"column_name", "data_type"});
+    }
+
+    private List<String> getTableNames(Connection connection) throws SQLException {
+        List<String> tableNames = new ArrayList<>();
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet tables = metaData.getTables(null, null, "%", new String[]{"TABLE"});
+
+        while (tables.next()) {
+            tableNames.add(tables.getString("TABLE_NAME"));
+        }
+
+        return tableNames;
+    }
+
     private Connection createConnection(DatabaseConfigModel dbConfig) throws SQLException {
         return DriverManager.getConnection(dbConfig.getConnectionUrl(), dbConfig.getUsername(), dbConfig.getPassword());
     }
@@ -82,90 +124,6 @@ public class BackupService {
                 ex.printStackTrace();
             }
         }
-    }
-
-    private List<String> getTableNames(Connection connection) throws SQLException {
-        List<String> tables = new ArrayList<>();
-        DatabaseMetaData metaData = connection.getMetaData();
-        ResultSet resultSet = metaData.getTables(null, null, "%", new String[]{"TABLE"});
-
-        while(resultSet.next()) {
-            tables.add(resultSet.getString("TABLE_NAME"));
-        }
-
-        return tables;
-    }
-
-    private List<Map<String, Object>> getTableData(Connection connection, String tableName) throws SQLException {
-        List<Map<String, Object>> rows = new ArrayList<>();
-        String query = "SELECT * FROM " + tableName;
-
-        try(Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query)) {
-
-            ResultSetMetaData metaData = resultSet.getMetaData();
-            int columnCount = metaData.getColumnCount();
-
-            while(resultSet.next()) {
-                Map<String, Object> row = new HashMap<>();
-                for(int i = 1; i <= columnCount; i++) {
-                    row.put(metaData.getColumnName(i), resultSet.getObject(i));
-                }
-                rows.add(row);
-            }
-        }
-
-        return rows;
-    }
-
-    private Map<String, String> getTableSchema(Connection connection, String tableName) throws SQLException {
-        Map<String, String> columnTypes = new HashMap<>();
-
-        String query = "SELECT * FROM " + tableName + " LIMIT 1";
-        try (Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(query)) {
-
-            ResultSetMetaData metaData = resultSet.getMetaData();
-            int columnCount = metaData.getColumnCount();
-
-            for (int i = 1; i <= columnCount; i++) {
-                String columnName = metaData.getColumnName(i);
-                String columnType = metaData.getColumnTypeName(i);
-                columnTypes.put(columnName, columnType);
-            }
-        }
-
-        return columnTypes;
-    }
-
-    private List<Map<String, Object>> getTableDataWithTypes(Connection connection, String tableName) throws SQLException {
-        List<Map<String, Object>> rows = new ArrayList<>();
-        Map<String, String> columnTypes = getTableSchema(connection, tableName);
-        String query = "SELECT * FROM " + tableName;
-
-        try (Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(query)) {
-
-            ResultSetMetaData metaData = resultSet.getMetaData();
-            int columnCount = metaData.getColumnCount();
-
-            while (resultSet.next()) {
-                Map<String, Object> row = new HashMap<>();
-                for (int i = 1; i <= columnCount; i++) {
-                    String columnName = metaData.getColumnName(i);
-                    String columnType = columnTypes.get(columnName);
-                    Object columnValue = resultSet.getObject(i);
-
-                    Map<String, Object> valueWithType = new HashMap<>();
-                    valueWithType.put("value", columnValue);
-                    valueWithType.put("type", columnType);
-                    row.put(columnName, valueWithType);
-                }
-                rows.add(row);
-            }
-        }
-
-        return rows;
     }
 
     private void cleanupBackupDirectory(String dateBackupDirectory) {
